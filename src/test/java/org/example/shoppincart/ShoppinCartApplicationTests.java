73447f32f664350ee46d2c51355172985ef74172
LasseThomsen <lasse.thomsen@hotmail.dk>
2024-03-20 14:27:56 +0100

    Initial commit

---
+++ src/test/java/org/example/shoppincart/ShoppinCartApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.shoppincart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppinCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
